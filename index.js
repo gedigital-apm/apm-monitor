bd9dc18727887eee288d078d4f806e8e04907296
Tran, Dewey (GE Digital) <dewey@ge.com>
2018-11-05 15:52:34 -0800

    add backwards-compatibility support for NewRelic

--- index.js
+++ index.js
@@ -4,33 +4,59 @@ function valueOf(param, defaultValue) {
     return (param === undefined) ? defaultValue : param;
 }
 
-var params = process.env;
+function useAppDynamics(services, params) {
 
-if ((valueOf(params.MONITORING_ENABLED, "true") !== "true") ||
-    (valueOf(params.APPDYNAMICS_ENABLED, "true") !== "true")) {
-    console.warn("Application monitoring is DISABLED.");
-    return;
+    var enabled = false;
+    for(var i = 0; i < services.length; i++) {
+        var service = services[i];
+        if (service.name && service.name.indexOf("appdynamics")) {
+            enabled = true;
+            break;
+        }
+    }
+
+    if (enabled) {
+        var options = {
+            debug:                valueOf(params.APPDYNAMICS_DEBUG,                  false),
+            controllerPort:       valueOf(params.APPDYNAMICS_CONTROLLER_PORT,        443),
+            controllerSslEnabled: valueOf(params.APPDYNAMICS_CONTROLLER_SSL_ENABLED, true)
+        };
+
+        require("appdynamics").profile(options);
+    }
 }
-else if(!params.APPDYNAMICS_AGENT_APPLICATION_NAME) {
-    console.error("Application monitoring is DISABLED because application name not set.");
-    return;
+
+function useNewRelic(services, params) {
+
+    if (params.ENABLE_NEWRELIC_MONITORING === "true") {
+        require("newrelic");
+    }
 }
 
 try {
-    var options = {
-        debug:                valueOf(params.APPDYNAMICS_DEBUG,                  false),
-        controllerPort:       valueOf(params.APPDYNAMICS_CONTROLLER_PORT,        443),
-        controllerSslEnabled: valueOf(params.APPDYNAMICS_CONTROLLER_SSL_ENABLED, true),
-        nodeName:             valueOf(params.APPDYNAMICS_AGENT_NODE_NAME,        "process")
-    };
 
-    if (!options.debug) {
-        options.debug = valueOf(params.MONITORING_DEBUG, false);
+    var params = process.env;
+    if (params.MONITORING_ENABLED === "false" || params.MONITORING_DISABLED === "true") {
+        console.warn("Application monitoring is DISABLED.");
+        return;
     }
 
-    require("appdynamics").profile(options);
+    var vcap = params.VCAP_SERVICES;
+    if (!vcap) {
+        console.warn("VCAP_SERVICES unavailable. Application monitoring will be DISABLED.");
+        return;
+    }
+
+    var services = vcap["user-provided"];
+    if (!services) {
+        console.warn("No 'user-provided' services in VCAP_SERVICES available. Application monitoring will be DISABLED.");
+        return;
+    }
+    
+    useAppDynamics(services, params);
+    useNewRelic(services, params);
 }
-catch(e) {
-    console.error("Unable to enable application monitoring.");
+catch(error) {
+    console.error("Error occurred while enabling application monitoring.");
     console.error("Error: " + JSON.stringify(e));
 }
