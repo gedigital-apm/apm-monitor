01a49402b93655711d3189847bbf268d45c665a0
Priyabrat <priyabrat.nanda@ge.com>
2020-02-25 10:24:31 -0800

    add analytics agents for node apps

--- index.js
+++ index.js
@@ -21,11 +21,20 @@ try {
         debug:                valueOf(params.APPDYNAMICS_DEBUG,                  false),
         controllerPort:       valueOf(params.APPDYNAMICS_CONTROLLER_PORT,        443),
         controllerSslEnabled: valueOf(params.APPDYNAMICS_CONTROLLER_SSL_ENABLED, true),
-        nodeName:             valueOf(params.APPDYNAMICS_AGENT_NODE_NAME,        "process")
+        nodeName:             valueOf(params.APPDYNAMICS_AGENT_NODE_NAME,        "process"),
+        analytics: {}
     };
 
     if (!options.debug) {
         options.debug = valueOf(params.MONITORING_DEBUG, false);
+    }
+
+    if (params.APPD_ANALYTICS_AGENT_HOST) {
+        options.analytics = {
+            host: valueOf(params.APPD_ANALYTICS_AGENT_HOST, null),
+            port: valueOf(params.APPD_ANALYTICS_AGENT_PORT ? parseInt(params.APPD_ANALYTICS_AGENT_PORT) : 443),
+            ssl: valueOf(params.APPD_ANALYTICS_AGENT_SSL_ENABLED, false)
+        }
     }
     
     require("appdynamics").profile(options);
